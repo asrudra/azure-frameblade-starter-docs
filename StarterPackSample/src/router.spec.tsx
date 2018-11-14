d9c6999bd42fcccb18a1f4239eb5bd30f5900faa
Ashish Rudra <asrudra@microsoft.com>
2018-11-13 16:33:25 -0800

    test coverage to 100%

--- StarterPackSample/src/router.spec.tsx
+++ StarterPackSample/src/router.spec.tsx
@@ -1,13 +1,31 @@
 import 'jest';
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Router } from './router';
+import * as EnvironmentService from './app/services/portalEnvironmentService';
 
 describe('router', () => {
     describe('snapshot', () => {
         it ('matches snapshot', () => {
             const wrapper = shallow(<Router/>);
+
             expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe('componentWillMount', () => {
+        it('calls EnvironmentService to get routeName', async (done) => {
+            const spy = jest.spyOn(EnvironmentService, 'getSetting').mockResolvedValue('my-route');
+
+            const wrapper  = mount(<Router/>);
+
+            expect(wrapper).toBeDefined();
+            expect((wrapper.state() as any).routeName).toEqual(''); // tslint:disable-line: no-any
+            expect(spy).toHaveBeenNthCalledWith(1, 'routeName');
+
+            await Promise.resolve();
+            expect((wrapper.state() as any).routeName).toEqual('my-route'); // tslint:disable-line: no-any
+            done();
         });
     });
 });
